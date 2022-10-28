3644f5cfc7526ffa8446654613675bd6cf1ff6e9
Faraz Ahmed <masterdot707@gmail.com>
2022-10-28 05:22:06 +0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ListMenu from "./ListMenu";
+import RightMenu from "./RightMenu";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-md-3">
+          <ListMenu />
+        </div>
+        <div className="col-md-9">
+          <RightMenu />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/ListMenu.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ListMenu = () => {
+  return (
+    <ul class="list-group">
+      <li class="list-group-item my-2 green h5 border-0">1st Flavor</li>
+      <li class="list-group-item my-2 green h5 border-0">2nd Flavor</li>
+      <li class="list-group-item my-2 green h5 border-0">3rd Flavor</li>
+      <li class="list-group-item my-2 green h5 border-0">Crust</li>
+      <li class="list-group-item my-2 green h5 border-0">Topping</li>
+      <li class="list-group-item my-2 green h5 border-0">Add Extra</li>
+    </ul>
+  );
+};
+
+export default ListMenu;
